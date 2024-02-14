d8fb17cdf5947064411190ab7bcef44e77d533b3
Kai Jun Tam <thek3@Kais-MacBook-Pro.local>
2024-02-14 16:45:09 +0800

    fix build error

--- app/component/Nav.tsx
+++ app/component/Nav.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 
 export default function Nav() {
     return (
@@ -5,7 +6,7 @@ export default function Nav() {
         <nav className="bg-white border-gray-200 dark:bg-gray-900">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
-                    <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
+                    <Image src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
                     <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">the K3</span>
                 </a>
                 <div className="hidden w-full md:block md:w-auto" id="navbar-default">
