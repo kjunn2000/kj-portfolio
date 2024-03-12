342abedf7b1645abe31540ff46a0e45f642c06f8
Kai Jun Tam <thek3@36.8.126.129.static.m1net.com.sg>
2024-03-12 19:03:20 +0800

    update dark theme to work

--- app/component/Landing.tsx
+++ app/component/Landing.tsx
@@ -1,9 +1,9 @@
 
 export default function Landing() {
     return (
-        <section className="bg-center bg-no-repeat bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/conference.jpg')] bg-gray-700 bg-blend-multiply">
+        <section className="bg-center bg-no-repeat bg-[url('https://github.com/kjunn2000/personal-portfolio-image/blob/main/IMG_6948.JPG?raw=true')] bg-gray-700 bg-blend-multiply">
             <div className="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
-            <h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Kai Jun</span> Software Engineer.</h1>
+            <h1 className="mb-4 text-3xl font-extrabold text-gray-900 text-white md:text-5xl lg:text-6xl"><span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Kai Jun</span> Software Engineer.</h1>
                 <p className="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">Explore my portfolio and witness the artistry of a dedicated software engineer. From diverse projects to innovative code, let turn ideas into digital reality together.</p>
                 <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
                     <a href="#footer" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
